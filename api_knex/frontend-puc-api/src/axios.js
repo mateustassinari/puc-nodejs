908d412b7ee233779918c0dcde0f798a95e41ecd
Mateus Tassinari Ferreira <mateustassinari18@gmail.com>
2023-05-15 19:42:52 -0300

    baseurl da api

--- api_knex/frontend-puc-api/src/axios.js
+++ api_knex/frontend-puc-api/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://api-node-knex-puc.onrender.com"
 });
 
 axiosInstance.interceptors.request.use((req) => {
